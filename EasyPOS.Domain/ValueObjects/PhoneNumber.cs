b24f9dd5aa555c1f7c5d5ae7db73c9b2f6498bcb
ArteMaxL <artemiolucero@gmail.com>
2024-03-19 22:52:39 -0300

    ValueObjects

---
+++ EasyPOS.Domain/ValueObjects/PhoneNumber.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.RegularExpressions;
+
+namespace EasyPOS.Domain.ValueObjects
+{
+    public partial record PhoneNumber
+    {
+        private const int DefaultLength = 9;
+        private const string Pattern = @"^(?:-*\d-*){8}$";
+        private PhoneNumber(string value) => Value = value;
+        public static PhoneNumber? Create(string value)
+        {
+            return !IsValidPhoneNumber(value) ? null : new PhoneNumber(value);
+        }
+        private string Value { get; init; }
+
+        [GeneratedRegex(Pattern)]
+        private static partial Regex PhoneNumberRegex();
+        private static bool IsValidPhoneNumber(string value)
+        {
+            return !string.IsNullOrEmpty(value) ||
+                PhoneNumberRegex().IsMatch(value) ||
+                value.Length == DefaultLength;
+        }
+    }
+}
